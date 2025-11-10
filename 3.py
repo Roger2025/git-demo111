52c0748541be9c556f47185ef854bad94eba11be
roger2022 <A127038349@gmail.com>
2025-11-10 21:36:22 +0800

    完成連續數值輸入版本

--- 3.py
+++ 3.py
@@ -1,9 +1,16 @@
-a = eval(input("請輸入a:"))
-b = eval(input("請輸入b:"))
+while True:
+    try:
+        a = input("請輸入a:")
+        if a == "exit":
+            break
+        a = eval(a)
+        b = eval(input("請輸入b:"))
 
-if a > b:
-    print(f"a比b大{a - b}")
-elif b > a:
-    print(f"b比a大{b - a}")
-else:
-    print("a跟b一樣大")
+        if a > b:
+            print(f"a比b大{a - b}")
+        elif b > a:
+            print(f"b比a大{b - a}")
+        else:
+            print("a跟b一樣大")
+    except Exception as e:
+        print("輸入錯誤:", e)
